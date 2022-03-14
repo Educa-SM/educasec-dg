b25806488a6c60cda688d2439fb0cf69693ed6cb
alonsodelcordova <alonsodelcordova@gmail.com>
2022-03-14 16:11:34 -0500

    ✨Add: Docente -> Administrations

--- apps/institucion/admin.py
+++ apps/institucion/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class DocenteResource (resources.ModelResource):
+   class Meta:
+      model = Docente
+      fields = ('nro_documento','nombres','apellido_paterno','apellido_materno','tipo_documento','user')
+
+class DocenteAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   search_fields = ['nro_documento','nombres','apellido_paterno','apellido_materno']
+   list_display =('nro_documento','tipo_documento','nombres','apellido_paterno','apellido_materno','user')
+   resources_class = DocenteResource
+
+admin.site.register(Docente, DocenteAdmin)
