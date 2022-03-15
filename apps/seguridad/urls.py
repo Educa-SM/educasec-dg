343cbff15e868ed63b095b564f7002474271be96
alonsodelcordova <alonsodelcordova@gmail.com>
2022-03-15 16:35:18 -0500

    ✨add: Logout view

--- apps/seguridad/urls.py
+++ apps/seguridad/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('docente/', RegisterDocenteView.as_view()),
     path('alumno/', RegisterAlumnoView.as_view()),
     path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
