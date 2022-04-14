e142b3085f08b4c20a95b849a92b535c58d0803c
alonsodelcordova <alonsodelcordova@gmail.com>
2022-04-13 19:11:49 -0500

    ✨feature: static bug

--- educasec/settings/production.py
+++ educasec/settings/production.py
@@ -45,3 +45,6 @@ CORS_ORIGIN_WHITELIST = (
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
