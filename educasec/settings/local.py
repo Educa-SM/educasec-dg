e4384b060df7952ece0dbf56628b34d5b2bb00b1
alonsodelcordova <alonsodelcordova@gmail.com>
2022-04-13 19:30:56 -0500

    ✨feature: update files

--- educasec/settings/local.py
+++ educasec/settings/local.py
@@ -23,6 +23,3 @@ CORS_ORIGIN_WHITELIST = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
