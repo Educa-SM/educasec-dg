97185a3049ee55c6e9abab6b1d60be68c913b11c
Educa <educa@vps.server.local>
2022-04-19 21:17:24 +0000

    Fixed Errors

--- educasec/settings/production.py
+++ educasec/settings/production.py
@@ -11,12 +11,12 @@ ALLOWED_HOSTS = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # conexion con postgresql
 """DATABASES = {
     'default': dj_database_url.config(
@@ -24,16 +24,16 @@ ALLOWED_HOSTS = [
     )
 }"""
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'NAME': 'educasec',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#        'NAME': 'educasec',
+#        'USER': 'postgres',
+#        'PASSWORD': '123456',
+#    }
+#}
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
