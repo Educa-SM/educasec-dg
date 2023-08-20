ca6eab1f3fbe7cf5c0c656c84d89fbd29af7c7ac
alonsodelcordova <alonsodelcordova@gmail.com>
2023-08-20 10:30:42 -0500

    reaload url

--- educasm/settings/local.py
+++ educasm/settings/local.py
@@ -10,15 +10,19 @@ ALLOWED_HOSTS = [
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = db.SQLITE
 
+CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+#CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
     'https://educasm-peru2.web.app',
     'https://sv-rvbfwpnmzd.cloud.elastika.pe',
-    'http://sv-rvbfwpnmzd.cloud.elastika.pe',    
-    'sv-rvbfwpnmzd.cloud.elastika.pe'
+    'http://sv-rvbfwpnmzd.cloud.elastika.pe'
 )
 
 CSRF_TRUSTED_ORIGINS = [
